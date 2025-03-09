6e2915bf5236741c174c5ea3f9375610eed58e7e
IronJam11 <aaryanjain888@gmail.com>
2025-03-09 23:19:28 +0530

    feat: add pages

---
+++ treee-frontend/lib/utils/constants.dart
@@ -0,0 +1,6 @@
+String alchemy_rpc_url =
+    "https://eth-sepolia.g.alchemy.com/v2/ghiIjYuaumHfkffONpzBEItpKXWt9952";
+    
+String CONTRACT_ADDRESS = "0x582Dd54E70823565A81A5464de784225907346f1";
+
+String OWNER_ADDRESS = "0x3592638Dbe19AF5005A847CC0881876c87B50D29";
